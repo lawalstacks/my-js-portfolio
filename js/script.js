c7dee17ef8d8a5ad3d0f5ffcaa87807e81840b06
Kazeem Lawal <quazeemastic@gmail.com>
2024-05-02 16:34:35 +0100

    Add dark mode toggle functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 /**
- * @copyright codewithsadee 2023
- * @author Sadee <codewithsadee24@gmail.com>
+ * @copyright lawalstacks.js 2024
+ * @author lawalstacks<quazeemastic@gmail.com>
  */
 
 "use strict";
+
+const $themeBtn = document.querySelector("[data-theme-btn");
+const $HTML = document.documentElement;
+
+let isDark = window.matchMedia("(prefers-color-scheme)").matches;
+
+if(sessionStorage.getItem("theme")){
+  $HTML.dataset.theme = sessionStorage.getItem("theme");
+}else{
+  $HTML.dataset.theme = isDark ? "dark" : "light";
+  sessionStorage.setItem("theme", $HTML.dataset.theme);
+}
+
+const changeTheme = () => {
+ $HTML.dataset.theme = sessionStorage.getItem("theme") = "light" ? "dark" : "light";
+  sessionStorage.setItem("theme",$HTML.dataset.theme);
+
+}
+
+$themeBtn.addEventListener("click", changeTheme);
