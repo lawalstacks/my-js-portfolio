ec20b1cd5e6152aa6ec20b7c0de261ae0e79a80a
lawalstacks <quazeemastic@gmail.com>
2024-05-04 15:17:33 +0100

    Add Footers and media queries

--- js/script.js
+++ js/script.js
@@ -24,3 +24,28 @@ $HTML.dataset.theme = sessionStorage.getItem("theme") === "light" ? "dark" : "li
 }
 
 $themeBtn.addEventListener("click", changeTheme);
+
+/**
+ * setting primary tabs and content interactive 
+ */
+ 
+ const $tabBtn = document.querySelectorAll("[data-tab-btn]");
+ 
+ let [lastActiveTab] = document.querySelectorAll("[data-tab-content]");
+ 
+ let [lastActiveTabBtn] = $tabBtn;
+ 
+ 
+ $tabBtn.forEach((item) => {
+   item.addEventListener('click', function(){
+   lastActiveTab.classList.remove("active");
+   lastActiveTabBtn.classList.remove("active");
+   
+   const $tabContent = document.querySelector(`[data-tab-content = "${item.dataset.tabBtn}"]`);
+   $tabContent.classList.add("active");
+   this.classList.add("active");
+   
+   lastActiveTab = $tabContent;
+   lastActiveTabBtn = this;
+   })
+ })
